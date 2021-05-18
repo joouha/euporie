e6e7a4e530461f35fda5f2da99a58b9063067ecf
Josiah Outram Halstead <josiah@halstead.email>
2021-05-18 08:51:11 +0100

    Lint and type check config.py

--- euporie/config.py
+++ euporie/config.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
+"""Defines a configuration class for euporie."""
 import json
 import logging
 import os
 import sys
 from functools import lru_cache
 from pathlib import Path
+from typing import Union
 
-import jsonschema
+import jsonschema  # type: ignore
 
 from euporie import _app_name
 
 log = logging.getLogger(__name__)
 
-CONFIG_SCHEMA = {
+CONFIG_SCHEMA: "dict" = {
     "title": "Euporie Configuration",
     "description": "A configuration for euporie",
     "type": "object",
@@ -62,16 +64,27 @@ CONFIG_SCHEMA = {
 
 
 class Config:
+    """A configuration object with configuration values available as attributes.
+
+    Default configuration variables are loaded from the defaults defined in the
+    schema, then overwritten with values defined in a configuration file.
+    """
+
     win = sys.platform.startswith("win") or (sys.platform == "cli" and os.name == "nt")
 
     def __init__(self):
+        """Ininitate the Configuration object."""
         super().__setattr__("valid", True)
         self.load_defaults()
         self.load_config_file()
 
-    @property
+    @property  # type: ignore
     @lru_cache
-    def config_file_path(self):
+    def config_file_path(self) -> "Path":
+        """Determine where the user's config file should be stored.
+
+        TODO: Consider replacing this with https://github.com/ActiveState/appdirs
+        """
         if self.win:
             path = (
                 Path(os.getenv("APPDATA", "~/appdata")).expanduser()
@@ -89,7 +102,9 @@ class Config:
         return path / "config.json"
 
     @property
-    def json_data(self):
+    def json_data(self) -> "dict[str, Union[str, bool, int]]":
+        """Attempts to load the configuration file json."""
+        assert isinstance(self.config_file_path, Path)
         if self.valid and self.config_file_path.exists():
             with open(self.config_file_path, "r") as f:
                 try:
@@ -115,11 +130,27 @@ class Config:
             log.warning("The configuration file was not loaded")
         return {}
 
-    def set(self, attr, value):
+    def set(self, attr: "str", value: "Union[bool, int, str]") -> "None":
+        """Set a configuration attribute.
+
+        Args:
+            attr: The name of the attribute to set
+            value: The value to give the attribute
+
+        """
         if hasattr(self, attr):
             setattr(self, attr, value)
 
-    def toggle(self, attr):
+    def toggle(self, attr: "str") -> "None":
+        """Switches attributes between permitted configuration states.
+
+        For boolean values, they are toggled between True and False. Integer values are
+        incremented and reset within the permitted range.
+
+        Args:
+            attr: The name of the attribute to toggle.
+
+        """
         if hasattr(self, attr):
             current = getattr(self, attr)
             schema = CONFIG_SCHEMA["properties"][attr]
@@ -133,22 +164,26 @@ class Config:
                     + (current - schema["minimum"] + 1) % (schema["maximum"] + 1),
                 )
 
-    def load_defaults(self):
+    def load_defaults(self) -> "None":
+        """Sets attributes from the default values in the JSON schema."""
         for key, schema in CONFIG_SCHEMA["properties"].items():
             super().__setattr__(key, schema["default"])
 
-    def load_config_file(self):
+    def load_config_file(self) -> "None":
+        """Sets attributes from JSON data loaded from the configuration file."""
         for key, value in self.json_data.items():
             super().__setattr__(key, value)
 
-    def __getattr__(self, name):
+    def __getattr__(self, name: "str") -> "Union[bool, int, str]":
         """Defined to prevent mypy attemptint to type check dynamic attributes."""
-        return super().__getattr__(name)
+        return getattr(self, name)
 
-    def __setattr__(self, name, value):
+    def __setattr__(self, name: "str", value: "Union[bool, int, str]") -> "None":
+        """Sets configuration attributes and writes their values to the config file."""
         if self.valid:
             json_data = self.json_data
             json_data[name] = value
+            assert isinstance(self.config_file_path, Path)
             with open(self.config_file_path, "w") as f:
                 json.dump(json_data, f, indent=2)
         return super().__setattr__(name, value)
