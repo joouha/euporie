471906fd3bd26ac7b5b59c35f57af65b12837a3c
Josiah Outram Halstead <josiah@halstead.email>
2021-05-21 12:26:13 +0100

    Updates to configuration class

--- euporie/config.py
+++ euporie/config.py
@@ -4,13 +4,11 @@ from __future__ import annotations
 
 import json
 import logging
-import os
-import sys
-from functools import lru_cache
 from pathlib import Path
-from typing import Union
+from typing import Any, Iterator, Union
 
 import jsonschema  # type: ignore
+from appdirs import user_config_dir  # type: ignore
 
 from euporie import _app_name
 
@@ -72,46 +70,24 @@ class Config:
     schema, then overwritten with values defined in a configuration file.
     """
 
-    win = sys.platform.startswith("win") or (sys.platform == "cli" and os.name == "nt")
+    defaults = {
+        key: schema["default"] for key, schema in CONFIG_SCHEMA["properties"].items()
+    }
+    conf_file_name = "config.json"
 
     def __init__(self):
         """Ininitate the Configuration object."""
-        super().__setattr__("valid", True)
-        self.load_defaults()
+        self.valid_user_conf = True
+        self.user_conf = {}
+        conf_dir = Path(user_config_dir(_app_name, appauthor=False))
+        conf_dir.mkdir(exist_ok=True, parents=True)
+        self.config_file_path = conf_dir / self.conf_file_name
         self.load_config_file()
 
-    @property  # type: ignore
-    @lru_cache
-    def config_file_path(self) -> "Path":
-        """Determine where the user's config file should be stored.
-
-        TODO: Consider replacing this with https://github.com/ActiveState/appdirs
-
-        Returns:
-            The path at which the config file should be stored.
-
-        """
-        if self.win:
-            path = (
-                Path(os.getenv("APPDATA", "~/appdata")).expanduser()
-                / "roaming"
-                / _app_name
-            )
-        elif sys.platform == "darwin":
-            path = Path("~/Library/Application Support/").expanduser() / _app_name
-        else:
-            path = (
-                Path(os.getenv("XDG_CONFIG_HOME", "~/.config")).expanduser() / _app_name
-            )
-        # Create config folder if it doesn't exist
-        path.mkdir(exist_ok=True, parents=True)
-        return path / "config.json"
-
-    @property
-    def json_data(self) -> "dict[str, Union[str, bool, int]]":
+    def load_config_file(self) -> "None":
         """Attempts to load the configuration file json."""
         assert isinstance(self.config_file_path, Path)
-        if self.valid and self.config_file_path.exists():
+        if self.valid_user_conf and self.config_file_path.exists():
             with open(self.config_file_path, "r") as f:
                 try:
                     json_data = json.load(f)
@@ -121,7 +97,7 @@ class Config:
                         f"{self.config_file_path}\n"
                         "Is it valid json?"
                     )
-                    super().__setattr__("valid", False)
+                    self.valid_user_conf = False
                 else:
                     try:
                         jsonschema.validate(instance=json_data, schema=CONFIG_SCHEMA)
@@ -130,69 +106,142 @@ class Config:
                             f"Error in config file: {self.config_file_path}\n"
                             f"{error}"
                         )
-                        super().__setattr__("valid", False)
+                        self.valid_user_conf = False
                     else:
-                        return json_data
+                        self.user_conf.update(json_data)
+                        return
             log.warning("The configuration file was not loaded")
-        return {}
 
-    def set(self, attr: "str", value: "Union[bool, int, str]") -> "None":
+    def get(self, name: "str") -> "Any":
+        """Access a configuration variable, falling back to the default value if unset.
+
+        Args:
+            name: The name of the attribute to access.
+
+        Returns:
+            The configuration variable value.
+
+        """
+        return self.user_conf.get(name, self.defaults.get(name))
+
+    def __getattr__(self, name: "str") -> "Union[bool, int, str]":
+        """Enables access of config elements via dotted attributes.
+
+        Args:
+            name: The name of the attribute to access.
+
+        Returns:
+            The configuration variable value.
+
+        """
+        return self.get(name)
+
+    def __setitem__(self, name: "str", value: "Any") -> "None":
         """Set a configuration attribute.
 
         Args:
-            attr: The name of the attribute to set
-            value: The value to give the attribute
+            name: The name of the attribute to set.
+            value: The value to give the attribute.
 
         """
-        if hasattr(self, attr):
-            setattr(self, attr, value)
+        self.user_conf[name] = value
+        if self.valid_user_conf:
+            with open(self.config_file_path, "w") as f:
+                json.dump(self.user_conf, f, indent=2)
 
-    def toggle(self, attr: "str") -> "None":
+    def __setattr__(self, attr: "str", value: "Union[bool, int, str]") -> "None":
+        """Sets configuration attributes and writes their values to the config file."""
+        if attr in self.defaults:
+            self.__setitem__(attr, value)
+        else:
+            return super().__setattr__(attr, value)
+
+    def __delitem__(self, name: "str") -> "None":
+        """Unset a user's configuration variable.
+
+        This removes a configuration setting from the user's configuration, so the
+        default value will be used.
+
+        Args:
+            name: The name of the attribute to unset.
+
+        Raises:
+            KeyError: When the configuration does not exist in the configuration
+                schema.
+
+        """
+        try:
+            del self.user_conf[name]
+        except KeyError:
+            raise KeyError(f"Variable not found in the user config: {name!r}")
+        else:
+            if self.valid_user_conf:
+                with open(self.config_file_path, "w") as f:
+                    json.dump(self.user_conf, f, indent=2)
+
+    @property
+    def all(self) -> "dict[str, Any]":
+        """Return a combined dictionary of user and default values.
+
+        Returns:
+            The combined dictionary.
+
+        """
+        d = dict(self.defaults)
+        d.update(self.user_conf)
+        return d
+
+    def __iter__(self) -> "Iterator[str]":
+        """Iterate over all configuration variable names.
+
+        Returns:
+            An iterable of the combined dictionary.
+
+        """
+        return iter(self.all)
+
+    def __len__(self) -> "int":
+        """Return the length of the combined user and default settings.
+
+        Returns:
+            The length of the combined dictionary.
+
+        """
+        return len(set().union(self.defaults, self.user_conf))
+
+    def __str__(self) -> "str":
+        """Represent the configuration as a string.
+
+        Returns:
+            A string representing the configuration.
+
+        """
+        return f"Config({self.all!r})"
+
+    __repr__ = __str__
+
+    def toggle(self, name: "str") -> "None":
         """Switches attributes between permitted configuration states.
 
         For boolean values, they are toggled between True and False. Integer values are
         incremented and reset within the permitted range.
 
         Args:
-            attr: The name of the attribute to toggle.
+            name: The name of the attribute to toggle.
 
         """
-        if hasattr(self, attr):
-            current = getattr(self, attr)
-            schema = CONFIG_SCHEMA["properties"][attr]
+        if name in self.defaults:
+            current = getattr(self, name)
+            schema = CONFIG_SCHEMA["properties"][name]
             if schema["type"] == "boolean":
-                setattr(self, attr, not current)
+                setattr(self, name, not current)
             elif schema["type"] == "integer":
                 setattr(
                     self,
-                    attr,
+                    name,
                     schema["minimum"]
                     + (current - schema["minimum"] + 1) % (schema["maximum"] + 1),
                 )
-
-    def load_defaults(self) -> "None":
-        """Sets attributes from the default values in the JSON schema."""
-        for key, schema in CONFIG_SCHEMA["properties"].items():
-            super().__setattr__(key, schema["default"])
-
-    def load_config_file(self) -> "None":
-        """Sets attributes from JSON data loaded from the configuration file."""
-        for key, value in self.json_data.items():
-            super().__setattr__(key, value)
-
-    def __getattr__(self, name: "str") -> "Union[bool, int, str]":
-        """Defined to prevent mypy attemptint to type check dynamic attributes."""
-        return getattr(self, name)
-
-    def __setattr__(self, name: "str", value: "Union[bool, int, str]") -> "None":
-        """Sets configuration attributes and writes their values to the config file."""
-        if self.valid:
-            json_data = self.json_data
-            json_data[name] = value
-            assert isinstance(self.config_file_path, Path)
-            with open(self.config_file_path, "w") as f:
-                json.dump(json_data, f, indent=2)
-        return super().__setattr__(name, value)
 
 
 config = Config()
