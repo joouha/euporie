10291fbb407b2d1dac554f5cb961e284285e588f
Josiah Outram Halstead <josiah@halstead.email>
2021-05-25 22:00:30 +0100

    Bump version number to 0.1.14

--- euporie/__init__.py
+++ euporie/__init__.py
@@ -2,7 +2,7 @@
 """Instantiates the euporie package."""
 
 __app_name__ = "europie"
-__version__ = "0.1.13"
+__version__ = "0.1.14"
 __logo__ = "⚈"
 __strapline__ = "A TUI editor for Jupyter notebooks"
 __author__ = "Josiah Outram Halstead"
