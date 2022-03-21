8527f431cecd0a59bee28458f375341642be679d
Josiah Outram Halstead <josiah@halstead.email>
2022-03-21 13:30:00 +0000

    Allow navigating between cells in edit mode

--- euporie/commands/cell.py
+++ euporie/commands/cell.py
@@ -6,12 +6,9 @@ from prompt_toolkit.filters import buffer_has_focus
 
 from euporie.app.current import get_tui_app as get_app
 from euporie.commands.registry import add
-from euporie.filters import cell_has_focus
+from euporie.filters import cell_has_focus, cursor_on_first_line, cursor_on_last_line
 
 log = logging.getLogger(__name__)
-
-
-# Cell
 
 
 @add(
@@ -36,3 +33,34 @@ def split_cell() -> "None":
     cell = get_app().cell
     if cell is not None:
         cell.split()
+
+
+@add(
+    keys=["up"],
+    filter=cell_has_focus & buffer_has_focus & cursor_on_first_line,
+    group="cell",
+)
+def edit_previous_cell() -> "None":
+    """Move the cursor up to the previous cell."""
+    nb = get_app().notebook
+    if nb is not None:
+        new_index = nb.cell.index - 1
+        cells = nb.rendered_cells()
+        if 0 <= new_index < len(cells):
+            cells[new_index].focus(position=-1)
+
+
+@add(
+    keys=["down"],
+    filter=cell_has_focus & buffer_has_focus & cursor_on_last_line,
+    group="cell",
+)
+def edit_next_cell() -> "None":
+    """Move the cursor down to the next cell."""
+    nb = get_app().notebook
+    if nb is not None:
+        new_index = nb.cell.index + 1
+        cells = nb.rendered_cells()
+        log.debug(new_index)
+        if 0 <= new_index < len(cells):
+            cells[new_index].focus(position=0)
