101fc200afb7f09e2d7b07380e247224cb9a9ed8
Josiah Outram Halstead <josiah@halstead.email>
2022-08-16 08:30:50 +0100

    Remove markdown to formatted text renderer

--- euporie/core/convert/formats/formatted_text.py
+++ euporie/core/convert/formats/formatted_text.py
@@ -14,35 +14,6 @@ if TYPE_CHECKING:
 
     from prompt_toolkit.formatted_text.base import StyleAndTextTuples
 
-MARKDOWN_ENHANCED = False
-
-'''
-@register(
-    from_="markdown",
-    to="formatted_text",
-)
-def markdown_to_ft(
-    data: "str",
-    width: "Optional[int]" = None,
-    height: "Optional[int]" = None,
-    fg: "Optional[str]" = None,
-    bg: "Optional[str]" = None,
-) -> "StyleAndTextTuples":
-    """Converts markdown to formatted text."""
-    global MARKDOWN_ENHANCED
-    if not MARKDOWN_ENHANCED:
-        from euporie.core.formatted_text.markdown_enhanced import (
-            enable_enchanced_markdown,
-        )
-
-        enable_enchanced_markdown()
-        MARKDOWN_ENHANCED = True
-
-    from euporie.core.formatted_text.markdown import Markdown
-
-    return to_formatted_text(Markdown(data, width=width))
-'''
-
 
 @register(
     from_="html",
