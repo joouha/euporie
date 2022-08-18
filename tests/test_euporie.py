afb7cde963437e961e33f2f31b6deec13d9d46fd
Josiah Outram Halstead <josiah@halstead.email>
2022-08-18 16:54:26 +0100

    Remove useless version number test

--- tests/test_euporie.py
+++
@@ -1,8 +0,0 @@
-"""Test cases for __init__ module."""
-
-from euporie.core import __version__
-
-
-def test_version() -> "None":
-    """Version is correct."""
-    assert __version__ == "2.0.0-dev"
