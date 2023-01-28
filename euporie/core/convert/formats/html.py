9fe6680b3962f6ba5e79ec0502a02e0829074e9a
Josiah Outram Halstead <josiah@halstead.email>
2023-01-28 17:12:46 +0000

    Reimplement markdown code-block highlighting and update markdown CSS

--- euporie/core/convert/formats/html.py
+++ euporie/core/convert/formats/html.py
@@ -5,8 +5,15 @@ from __future__ import annotations
 import logging
 from typing import TYPE_CHECKING
 
+from markdown_it import MarkdownIt
+from mdit_py_plugins.amsmath import amsmath_plugin
+from mdit_py_plugins.dollarmath.index import dollarmath_plugin
+from mdit_py_plugins.texmath.index import texmath_plugin
+from pygments import highlight
+from pygments.formatters import HtmlFormatter
+from pygments.lexers import get_lexer_by_name
+
 from euporie.core.convert.base import register
-from euporie.core.convert.utils import have_modules
 from euporie.core.current import get_app
 
 if TYPE_CHECKING:
@@ -16,46 +23,44 @@ if TYPE_CHECKING:
 
 log = logging.getLogger(__name__)
 
-# Check for markdown-it-py
-markdown_parser: "Optional[MarkdownIt]" = None
 
-try:
-    from markdown_it import MarkdownIt
-except ModuleNotFoundError:
-    pass
-else:
+class MarkdownParser(MarkdownIt):
+    """Subclass the markdown parser to allow ``file:`` URIs."""
 
-    class MarkdownParser(MarkdownIt):
-        """Subclass the markdown parser to allow ``file:`` URIs."""
+    def validateLink(self, url: "str") -> "bool":
+        """Allows all link URIs."""
+        return True
 
-        def validateLink(self, url: "str") -> "bool":
-            """Allows all link URIs."""
-            return True
 
-    markdown_parser = (
-        MarkdownParser().enable("linkify").enable("table").enable("strikethrough")
+markdown_parser = (
+    (
+        MarkdownParser(
+            options_update={
+                "highlight": lambda text, language, lang_args: highlight(
+                    text,
+                    get_lexer_by_name(language),
+                    HtmlFormatter(
+                        nowrap=True,
+                        noclasses=True,
+                        style=app.config.syntax_theme
+                        if hasattr((app := get_app()), "config")
+                        else "default",
+                    ),
+                )
+            }
+        )
+        .enable("linkify")
+        .enable("table")
+        .enable("strikethrough")
     )
-
-# Check for markdown-it-py plugins
-try:
-    import mdit_py_plugins  # noqa F401
-except ModuleNotFoundError:
-    pass
-else:
-    from mdit_py_plugins.amsmath import amsmath_plugin
-    from mdit_py_plugins.dollarmath.index import dollarmath_plugin
-    from mdit_py_plugins.texmath.index import texmath_plugin
-
-    # from mdit_py_plugins.tasklists import tasklists_plugin
-
-    if markdown_parser is not None:
-        markdown_parser.use(texmath_plugin)
-        markdown_parser.use(dollarmath_plugin)
-        markdown_parser.use(amsmath_plugin)
-        # markdown_parser.use(tasklists_plugin)
+    .use(texmath_plugin)
+    .use(dollarmath_plugin)
+    .use(amsmath_plugin)
+    # .use(tasklists_plugin)
+)
 
 
-@register(from_="markdown", to="html", filter_=have_modules("markdown_it"))
+@register(from_="markdown", to="html")
 def markdown_to_html_markdown_it(
     data: "str|bytes",
     width: "Optional[int]" = None,
