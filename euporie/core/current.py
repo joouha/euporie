153ddce747784d7d402aeaf344d085bfd058fe21
Josiah Outram Halstead <josiah@halstead.email>
2023-05-10 13:07:01 +0100

    Change `get_app` to fallback to returning instance of `BaseApp` instead of `DummyApp`

--- euporie/core/current.py
+++ euporie/core/current.py
@@ -2,15 +2,23 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, cast
+from typing import TYPE_CHECKING
 
-from prompt_toolkit.application.current import get_app as ptk_get_app
+from prompt_toolkit.application.current import _current_app_session
 
 if TYPE_CHECKING:
+    from typing import Any
+
     from euporie.core.app import BaseApp
 
 
 def get_app() -> BaseApp:
-    """Get the current application."""
-    app = ptk_get_app()
-    return cast("BaseApp", app)
+    """Get the current active (running) Application."""
+    session = _current_app_session.get()
+    if session.app is not None:
+        return session.app
+
+    # Use a baseapp as our "DummyApplication"
+    from euporie.core.app import BaseApp
+
+    return BaseApp()
