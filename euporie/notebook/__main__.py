e77d305db4e0ef7ab09b401ac447a936737ffb0a
Josiah Outram Halstead <josiah@halstead.email>
2023-05-16 15:36:58 +0100

    Improve launchability of apps

--- euporie/notebook/__main__.py
+++ euporie/notebook/__main__.py
@@ -3,9 +3,11 @@
 
 def main() -> "None":
     """Call the main entrypoint to the application."""
+    import sys
+
     from euporie.core import __main__
 
-    __main__.main(__name__.split(".")[1])
+    __main__.main(str(sys.modules[__name__].__package__).rpartition(".")[2])
 
 
 if __name__ == "__main__":
