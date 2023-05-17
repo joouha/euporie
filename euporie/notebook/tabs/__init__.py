b1dcacd934096ba6119c3dc8ad5f69a86716e5c4
Josiah Outram Halstead <josiah@halstead.email>
2023-05-17 21:59:02 +0100

    Add JSON viewer tab

--- euporie/notebook/tabs/__init__.py
+++ euporie/notebook/tabs/__init__.py
@@ -2,8 +2,9 @@
 
 from euporie.notebook.tabs.display import DisplayTab
 from euporie.notebook.tabs.edit import EditorTab
+from euporie.notebook.tabs.json import JsonTab
 from euporie.notebook.tabs.log import LogView
 from euporie.notebook.tabs.notebook import Notebook
 from euporie.web.tabs.web import WebTab
 
-__all__ = ["DisplayTab", "EditorTab", "LogView", "Notebook", "WebTab"]
+__all__ = ["DisplayTab", "EditorTab", "JsonTab", "LogView", "Notebook", "WebTab"]
