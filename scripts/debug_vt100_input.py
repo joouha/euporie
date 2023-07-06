85a473e1e807cfe2e8e04a19b55ee90eca24cf5b
Josiah Outram Halstead <josiah@halstead.email>
2023-07-06 09:38:26 +0100

    Typing fixes

--- scripts/debug_vt100_input.py
+++ scripts/debug_vt100_input.py
@@ -6,13 +6,13 @@ from __future__ import annotations
 import sys
 
 from prompt_toolkit.input.vt100 import raw_mode
-from prompt_toolkit.key_binding import KeyPressEvent
+from prompt_toolkit.key_binding import KeyPress
 
 from euporie.core.io import Vt100Parser
 from euporie.core.keys import Keys
 
 
-def callback(key_press: KeyPressEvent) -> None:
+def callback(key_press: KeyPress) -> None:
     """Run when a key press event is received."""
     print(key_press)
     if key_press.key == Keys.ControlC:
