a8e4c7831162a7b55a9c81e87a6355e9f6967a7c
Josiah Outram Halstead <josiah@halstead.email>
2023-12-21 16:58:40 +0000

    Add `run_notebook` script to repo

---
+++ scripts/run_notebook.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""Script which runs the euporie-notebook app.
+
+Useful for profiles which cannot run a python module.:w
+"""
+from euporie.notebook.app import NotebookApp
+
+NotebookApp.launch()
