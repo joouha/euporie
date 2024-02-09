1e89b182807ca80cffd0c2176d5eb5d4b6578f4d
Josiah Outram Halstead <josiah_halstead@epam.com>
2024-02-09 17:54:13 +0000

    Implement diagnostic processor

--- euporie/core/processors.py
+++ euporie/core/processors.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 import logging
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, cast
 
 from prompt_toolkit.layout.processors import (
     AppendAutoSuggestion,
@@ -17,7 +17,10 @@ if TYPE_CHECKING:
     from typing import Callable
 
     from prompt_toolkit.data_structures import Point
+    from prompt_toolkit.formatted_text.base import StyleAndTextTuples
     from prompt_toolkit.layout.processors import TransformationInput
+
+    from euporie.core.diagnostics import Report
 
 
 log = logging.getLogger(__name__)
@@ -64,6 +67,56 @@ class ShowTrailingWhiteSpaceProcessor(Processor):
                     fragments[i] = (f"{style} {self.style}", new_char)
                 else:
                     break
+        return Transformation(fragments)
+
+
+class DiagnosticProcessor(Processor):
+    """Highlight diagnostics."""
+
+    def __init__(
+        self,
+        report: Report | Callable[[], Report],
+        style: str = "underline",
+    ) -> None:
+        """Create a new processor instance."""
+        self._report = report
+        self.style = style
+
+    @property
+    def report(self) -> Report:
+        """Return the current diagnostics report."""
+        if callable(self._report):
+            return self._report()
+        return self._report
+
+    def apply_transformation(self, ti: TransformationInput) -> Transformation:
+        """Underline the text ranges relating to diagnostics in the report."""
+        line = ti.lineno
+        fragments = ti.fragments
+        self_style = self.style
+        for item in self.report:
+            if item.lines.start < line < item.lines.stop - 1:
+                fragments = cast(
+                    "StyleAndTextTuples",
+                    [
+                        (f"{style} {self.style}", text, *rest)
+                        for style, text, *rest in fragments
+                    ],
+                )
+            elif line == item.lines.start or line == item.lines.stop - 1:
+                fragments = explode_text_fragments(fragments)
+                start = item.chars.start if line == item.lines.start else 0
+                end = (
+                    item.chars.stop - 1
+                    if line == item.lines.stop - 1
+                    else len(fragments)
+                )
+                for i in range(start, min(len(fragments), end)):
+                    fragments[i] = (
+                        f"{fragments[i][0]} {self_style}",
+                        *fragments[i][1:],
+                    )
+
         return Transformation(fragments)
 
 
