be9b26a4197deaea51325fe792125a697837623f
Josiah Outram Halstead <josiah@halstead.email>
2024-02-09 21:13:50 +0000

    Add known LSP server list generation script

---
+++ scripts/clone_helix_lsp_config.py
@@ -0,0 +1,31 @@
+"""Clone Helix editor's list of known LSP servers."""
+
+import tomllib
+from upath import UPath
+
+source = UPath("https://github.com/helix-editor/helix/raw/master/languages.toml")
+with source.open("rb") as f:
+    c = tomllib.load(f)
+
+lsps = c.get("language-server", {})
+
+for lsp in lsps.values():
+    # Margs command and args
+    lsp["cmd"] = [lsp["command"], *lsp.get("args", [])]
+    del lsp["command"]
+    if "args" in lsp:
+        del lsp["args"]
+    # Rename settings to config
+    if "settings" in lsp:
+        lsp["config"] = lsp["settings"]
+        del lsp["settings"]
+
+# Assign languages to LSPs
+for lang in c.get("language", []):
+    for name in lang.get("language-servers", []):
+        if lsp := lsps.get(name, {}):
+            if "languages" not in lsp:
+                lsp["languages"] = set()
+            lsp["languages"].add(lang["name"])
+
+print(lsps)
