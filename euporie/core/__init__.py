2d1cc3884dea9bc4d3640263bc3979879ec2ea9d
Josiah Outram Halstead <josiah_halstead@epam.com>
2024-03-01 23:41:00 +0000

    Release v2.8.1

--- euporie/core/__init__.py
+++ euporie/core/__init__.py
@@ -1,7 +1,7 @@
 """This package defines the euporie application and its components."""
 
 __app_name__ = "euporie"
-__version__ = "2.8.0"
+__version__ = "2.8.1"
 __logo__ = "⚈"
 __strapline__ = "Jupyter in the terminal"
 __author__ = "Josiah Outram Halstead"
