39a8a7be966ed05af92e9b829e6d082eaa2e16c8
Josiah Outram Halstead <josiah@halstead.email>
2024-10-04 10:00:32 +0100

    Add script to run preview app

---
+++ scripts/run_preview.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""Script which runs the euporie-notebook app.
+
+Useful for profiles which cannot run a python module.:w
+"""
+
+from euporie.preview.app import PreviewApp
+
+PreviewApp.launch()
