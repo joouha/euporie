2771e419cd1199a8c73a89c518253ec7d4820527
Josiah Outram Halstead <josiah@halstead.email>
2024-10-29 15:51:50 +0000

    Fixes to tab registry

--- euporie/notebook/tabs/__init__.py
+++ euporie/notebook/tabs/__init__.py
@@ -7,27 +7,33 @@ _TAB_REGISTRY.extend(
     [
         TabRegistryEntry(
             path="euporie.notebook.tabs.display:DisplayTab",
+            name="File Viewer",
             mime_types=set(MIME_FORMATS.keys()),
         ),
         TabRegistryEntry(
             path="euporie.notebook.tabs.edit:EditorTab",
+            name="Text Editor",
             mime_types={"text/*"},
             weight=1,
         ),
         TabRegistryEntry(
             path="euporie.notebook.tabs.json:JsonTab",
+            name="JSON Viewer",
             mime_types={"*json"},
             file_extensions={".json": None},
         ),
         TabRegistryEntry(
             path="euporie.notebook.tabs.notebook:Notebook",
+            name="Notebook Editor",
             mime_types={"application/x-ipynb+json"},
             file_extensions={".ipynb": None},
             weight=3,
         ),
         TabRegistryEntry(
             path="euporie.web.tabs.web:WebTab",
+            name="Web Viewer",
             mime_types={"text/html", "text/markdown"},
+            weight=2,
         ),
     ]
 )
@@ -37,6 +43,7 @@ try:
 except ModuleNotFoundError:
     pass
 else:
-    _TAB_REGISTRY["euporie.notebook.tabs.notebook:Notebook"]["file_extensions"].update(
-        dict.fromkeys(NOTEBOOK_EXTENSIONS)
-    )
+    for entry in _TAB_REGISTRY:
+        if entry.path == "euporie.notebook.tabs.notebook:Notebook":
+            entry.file_extensions.update(dict.fromkeys(NOTEBOOK_EXTENSIONS))
+            break
