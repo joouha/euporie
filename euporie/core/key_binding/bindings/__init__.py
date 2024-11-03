2e0a5ea14a9283f06ca6b2b04852788a4f62e1c4
Your Name <josiah_halstead@epam.com>
2024-11-03 12:38:27 +0000

    Ensure key-bindings are loaded

--- euporie/core/key_binding/bindings/__init__.py
+++ euporie/core/key_binding/bindings/__init__.py
@@ -1,1 +1,5 @@
 """Define collections of generic key-bindings which do not belong to widgets."""
+
+from . import basic, completion, micro, mouse, page_navigation, vi
+
+__all__ = ["basic", "completion", "micro", "mouse", "page_navigation", "vi"]
