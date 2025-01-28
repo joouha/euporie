16bea39911fe2566d1167997bb5d9191c7702ae4
Your Name <josiah_halstead@epam.com>
2025-01-28 21:21:46 +0000

    fix: Prevent 405 Method Not Allowed error when opening some URLs

--- euporie/core/path.py
+++ euporie/core/path.py
@@ -9,36 +9,72 @@ from typing import TYPE_CHECKING
 import upath
 from aiohttp.client_reqrep import ClientResponse
 from fsspec.implementations.http import HTTPFileSystem as FsHTTPFileSystem
+from fsspec.implementations.http import get_client
 from fsspec.registry import register_implementation as fs_register_implementation
 from upath import UPath
 
 if TYPE_CHECKING:
+    import asyncio
     from collections.abc import Mapping
     from os import PathLike
-    from typing import Any
+    from typing import Any, Callable
+
+    import aiohttp
 
 
 log = logging.getLogger(__name__)
 
 
-# Monkey-patch `aiohttp` to not raise exceptions on non-200 responses
+class NoRaiseClientResponse(ClientResponse):
+    """An ``aiohttp`` client response which does not raise on >=400 status responses."""
 
-
-def _raise_for_status(self: ClientResponse) -> None:
-    """Monkey-patch :py:class:`aiohttp.ClientResponse` not to raise for any status."""
-
-
-setattr(ClientResponse, "raise_for_status", _raise_for_status)  # noqa B010
-
-
-# Define and register non-raising HTTP filesystem implementation for fsspec
+    @property
+    def ok(self) -> bool:
+        """Returns ``True`` if ``status`` is probably renderable."""
+        return self.status not in {405}
 
 
 class HTTPFileSystem(FsHTTPFileSystem):
-    """A :py:class:`HTTPFileSystem` which does not raise exceptions on 404 errors."""
+    """A HTTP filesystem implementation which does not raise on errors."""
+
+    def __init__(
+        self,
+        simple_links: bool = True,
+        block_size: int | None = None,
+        same_scheme: bool = True,
+        size_policy: None = None,
+        cache_type: str = "bytes",
+        cache_options: dict[str, Any] | None = None,
+        asynchronous: bool = False,
+        loop: asyncio.AbstractEventLoop | None = None,
+        client_kwargs: dict[str, Any] | None = None,
+        get_client: Callable[..., aiohttp.ClientSession] = get_client,
+        encoded: bool = False,
+        **storage_options: Any,
+    ) -> None:
+        """Defaults to using :py:mod:`NoRaiseClientResponse` for responses."""
+        client_kwargs = {
+            "response_class": NoRaiseClientResponse,
+            **(client_kwargs or {}),
+        }
+        super().__init__(
+            simple_links=simple_links,
+            block_size=block_size,
+            same_scheme=same_scheme,
+            size_policy=size_policy,
+            cache_type=cache_type,
+            cache_options=cache_options,
+            asynchronous=asynchronous,
+            loop=loop,
+            client_kwargs=client_kwargs,
+            get_client=get_client,
+            encoded=encoded,
+            **storage_options,
+        )
 
     def _raise_not_found_for_status(self, response: ClientResponse, url: str) -> None:
         """Do not raise an exception for 404 errors."""
+        response.raise_for_status()
 
 
 fs_register_implementation("http", HTTPFileSystem, clobber=True)
