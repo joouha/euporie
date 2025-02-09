040077753c92beb5a72a87fc78c048912ca87309
Josiah Outram Halstead <josiah@halstead.email>
2025-02-09 14:27:37 +0000

    Fix tests

--- tests/test_core_completions.py
+++ tests/test_core_completions.py
@@ -2,8 +2,7 @@
 
 from __future__ import annotations
 
-import asyncio
-from unittest.mock import Mock
+from unittest.mock import AsyncMock, Mock
 
 from prompt_toolkit.completion import CompleteEvent, Completion
 from prompt_toolkit.document import Document
@@ -15,8 +14,8 @@ from euporie.core.kernel.base import BaseKernel
 def test_kernel_completer_init() -> None:
     """The kernel completer saves a reference to the passed kernel on init."""
     kernel = Mock(spec=BaseKernel)
-    completer = KernelCompleter(kernel)
-    assert completer.kernel == kernel
+    completer = KernelCompleter(lambda: kernel)
+    assert completer._kernel() == kernel
 
 
 def test_kernel_completer_get_completions() -> None:
@@ -28,27 +27,18 @@ def test_kernel_completer_get_completions() -> None:
     assert not completions
 
 
-def test_kernel_completer_get_completions_async() -> None:
+async def test_kernel_completer_get_completions_async() -> None:
     """The asynchronous "get_completions" method yields completions."""
-
-    async def mock_complete_async(
-        source: str, cursor_pos: int, timeout: int = 60
-    ) -> list[dict]:
-        return [{"text": "print"}]
-
-    kernel = Mock(spec=BaseKernel, complete_async=mock_complete_async)
-    completer = KernelCompleter(kernel)
+    kernel = AsyncMock(spec=BaseKernel)
+    kernel.complete_async = AsyncMock()
+    kernel.complete_async.return_value = [{"text": "print"}]
+    completer = KernelCompleter(lambda: kernel)
     document = Document("prin", cursor_position=4)
 
-    async def get_results() -> list[Completion]:
-        return [
-            result
-            async for result in completer.get_completions_async(
-                document, CompleteEvent()
-            )
-        ]
-
-    completions = asyncio.run(get_results())
+    completions = [
+        result
+        async for result in completer.get_completions_async(document, CompleteEvent())
+    ]
 
     assert list(completions) == [
         Completion(
