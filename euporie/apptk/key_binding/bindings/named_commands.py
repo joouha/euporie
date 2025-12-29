dace6153276d63cd0ec56e94ec5d489d2a805a59
Josiah Outram Halstead <josiah@halstead.email>
2025-12-29 11:51:29 +0000

    Port extra completion commands to `apptk`

--- euporie/apptk/key_binding/bindings/named_commands.py
+++ euporie/apptk/key_binding/bindings/named_commands.py
@@ -8,13 +8,42 @@ from __future__ import annotations
 from prompt_toolkit.key_binding.bindings.named_commands import _readline_commands
 
 from euporie.apptk.commands import add_cmd, get_cmd
+from euporie.apptk.filters import (
+    buffer_has_focus,
+    completion_is_selected,
+    has_selection,
+)
+from euporie.apptk.filters.buffer import cursor_in_leading_ws
+from euporie.apptk.filters.modes import insert_mode
 
 __all__ = ["get_by_name"]
-
-# Convert legacy named commands into Commands
-for name, binding in _readline_commands.items():
-    add_cmd(name=name)(binding.handler)
 
 # Replace legacy named-command access methods
 register = add_cmd
 get_by_name = get_cmd
+
+# Comvert existing readline commands to apptk commands
+add_cmd(
+    filter=buffer_has_focus & insert_mode & ~has_selection & ~cursor_in_leading_ws,
+    hidden=True,
+    name="next-completion",
+    aliases=["menu-complete"],
+    description="Show the completion menu and select the next completion.",
+)(_readline_commands.pop("menu-complete").handler)
+
+
+add_cmd(
+    filter=buffer_has_focus
+    & completion_is_selected
+    & insert_mode
+    & ~has_selection
+    & ~cursor_in_leading_ws,
+    hidden=True,
+    name="previous-completion",
+    aliases=["menu-complete-backward"],
+    description="Show the completion menu and select the previous completion.",
+)(_readline_commands.pop("menu-complete-backward").handler)
+
+# Convert legacy named commands into Commands
+for name, binding in _readline_commands.items():
+    add_cmd(name=name)(binding.handler)
