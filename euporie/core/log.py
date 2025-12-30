7e45077655da1233b4c9e0cb39d9e142ed23bd48
Josiah Outram Halstead <josiah@halstead.email>
2025-12-30 09:39:53 +0000

    Port `stdout_to_log` to `apptk`

--- euporie/core/log.py
+++ euporie/core/log.py
@@ -7,7 +7,6 @@ import logging.config
 import sys
 import textwrap
 from collections import deque
-from io import StringIO
 from pathlib import Path
 from typing import TYPE_CHECKING, ClassVar
 
@@ -345,52 +344,6 @@ class StdoutFormatter(FtFormatter):
             )
         output += [("", "\n")]
         return FormattedText(output)
-
-
-class stdout_to_log:
-    """A context manager which captures standard output and logs it."""
-
-    def __init__(
-        self, log: logging.Logger, output: str = "Literal['stdout','stderr']"
-    ) -> None:
-        """Create a new instance of the capturing context manager.
-
-        Args:
-            log: The logger to send the output to
-            output: Whether to capture the standard output or the standard error
-
-        """
-        self.log = log
-        self.out = StringIO()
-        self.output = output
-        self._original: TextIO | None = None
-
-    def __enter__(self) -> None:
-        """Intercept the standard output when entering the context manager."""
-        if self.output == "stderr":
-            self._original = sys.stderr
-            sys.stderr = self.out
-        else:
-            self._original = sys.stdout
-            sys.stdout = self.out
-
-    def __exit__(
-        self,
-        exc_type: type[BaseException] | None,
-        exc_value: BaseException | None,
-        exc_traceback: TracebackType | None,
-    ) -> None:
-        """Replace the standard output, and logs the captured output."""
-        assert self._original is not None
-        if self.output == "stderr":
-            sys.stderr = self._original
-        else:
-            sys.stdout = self._original
-        if exc_type is not None:
-            self.out.seek(0)
-            for line in self.out.readlines():
-                self.log.debug(str(line).strip())
-        self.out.close()
 
 
 def handle_exception(
